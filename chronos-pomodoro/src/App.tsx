dcc1c02d57c7de779bed776088509e5c4a6e5e14
DiogoBlanco <diogoblanco@outlook.com.br>
2025-12-14 21:16:07 -0300

    first component

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,7 +1,17 @@
-import './App.css';
+// PascalCase
 
-function App() {
-  return <div></div>;
+export function App() {
+  return (
+    <>
+      {' '}
+      // React Fragment
+      <h1>Olá Mundo!</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis
+        adipisci fuga quo nostrum, harum, quisquam suscipit libero aspernatur
+        exercitationem fugiat aliquam praesentium perspiciatis temporibus
+        explicabo eos corporis! Excepturi, impedit. Cupiditate!
+      </p>
+    </>
+  );
 }
-
-export default App;
