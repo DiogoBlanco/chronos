e610a1920992437c7744102e0e31e693a2a9f98d
DiogoBlanco <diogoblanco@outlook.com.br>
2025-12-21 20:36:51 -0300

    menu component

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,23 +1,20 @@
 // PascalCase
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
 import './styles/theme.css';
 import './styles/global.css';
-import './components/Container';
-import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
       <Container>
-        <section>LOGO</section>
+        <Logo />
       </Container>
+
       <Container>
-        <section>MENU</section>
-      </Container>
-      <Container>
-        <section>FORM</section>
-      </Container>
-      <Container>
-        <section>FOOTER</section>
+        <Menu />
       </Container>
     </>
   );
