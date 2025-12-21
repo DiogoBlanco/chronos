80c6e3ca47ba5cfc46f8dfdc2079150161ebabf5
DiogoBlanco <diogoblanco@outlook.com.br>
2025-12-21 12:48:23 -0300

    container component

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,30 +1,8 @@
 // PascalCase
 import './styles/theme.css';
 import './styles/global.css';
+import './components/Container';
 
 export function App() {
-  return (
-    <>
-      <div className='container'>
-        <div className='content'>
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FORM</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FOOTER</section>
-        </div>
-      </div>
-    </>
-  );
+  return <></>;
 }
