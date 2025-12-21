d95bb7b114ffeba8ab0013d046140be73ffadd85
DiogoBlanco <diogoblanco@outlook.com.br>
2025-12-21 17:28:28 -0300

    aula21

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -2,7 +2,23 @@
 import './styles/theme.css';
 import './styles/global.css';
 import './components/Container';
+import { Container } from './components/Container';
 
 export function App() {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <section>LOGO</section>
+      </Container>
+      <Container>
+        <section>MENU</section>
+      </Container>
+      <Container>
+        <section>FORM</section>
+      </Container>
+      <Container>
+        <section>FOOTER</section>
+      </Container>
+    </>
+  );
 }
